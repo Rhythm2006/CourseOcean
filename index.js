405664e2345c6d3dc00d3578ab1002577f3d4865
Rhythm <rithimesh@gmail.com>
2026-02-07 00:06:43 +0530

    Commit 4

--- index.js
+++ index.js
@@ -4,5 +4,9 @@ const app = express();
 const  { usersRouter } = require("./Routes/users")
 const { coursesRouter } = require("./Routes/course"); 
 
+app.use("/api/v1/user",usersRouter);
+app.use("/api/v1/course",coursesRouter);
+
+
 
 app.listen(3000); 
